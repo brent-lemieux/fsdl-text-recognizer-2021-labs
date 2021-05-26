68bda95a0c60bff916c9d208e24acb07dc78f9a9
Brent Lemieux <brent.lemieux@ibotta.com>
2021-05-26 10:15:06 -0600

    lab2 strides

--- lab2/text_recognizer/models/cnn.py
+++ lab2/text_recognizer/models/cnn.py
@@ -9,20 +9,31 @@ import torch.nn.functional as F
 CONV_DIM = 64
 FC_DIM = 128
 IMAGE_SIZE = 28
+STRIDE = 2
+DROPOUT = .1
 
 
 class ConvBlock(nn.Module):
     """
-    Simple 3x3 conv with padding size 1 (to leave the input size unchanged), followed by a ReLU.
+    ResNet BasicBlock.
     """
-
-    def __init__(self, input_channels: int, output_channels: int) -> None:
+    def __init__(self, input_channels: int, output_channels: int,
+                 kernel_size: int = 3, stride: int = 1, dilation: int = 1) -> None:
         super().__init__()
-        self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(output_channels, output_channels, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=kernel_size, stride=stride, padding=dilation, dilation=dilation)
+        self.conv2 = nn.Conv2d(output_channels, output_channels, kernel_size=kernel_size, stride=1, dilation=1, padding=1)
         self.bn1 = nn.BatchNorm2d(output_channels)
         self.bn2 = nn.BatchNorm2d(output_channels)
         self.relu = nn.ReLU()
+
+        self.expansion = 1
+        if stride > 1:
+          self.downsample = nn.Sequential(
+              nn.Conv2d(input_channels, output_channels * self.expansion, kernel_size=1, stride=stride),
+              nn.BatchNorm2d(output_channels * self.expansion),
+          )
+        else:
+          self.downsample = None
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -41,6 +52,8 @@ class ConvBlock(nn.Module):
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
+        if self.downsample != None:
+          x = self.downsample(x)
         out += x
         out = self.relu(out)
         return out
@@ -58,11 +71,13 @@ class CNN(nn.Module):
 
         conv_dim = self.args.get("conv_dim", CONV_DIM)
         fc_dim = self.args.get("fc_dim", FC_DIM)
+        dropout = self.args.get("dropout", DROPOUT)
+        stride = self.args.get("stride", STRIDE)
 
-        self.conv1 = ConvBlock(input_dims[0], conv_dim)
-        self.conv2 = ConvBlock(conv_dim, conv_dim)
-        self.dropout = nn.Dropout(0.25)
-        self.max_pool = nn.MaxPool2d(2)
+        self.conv1 = ConvBlock(input_dims[0], conv_dim, stride=stride)
+        self.conv2 = ConvBlock(conv_dim, conv_dim, stride=stride)
+        self.dropout = nn.Dropout(dropout)
+        # self.max_pool = nn.MaxPool2d(2)
 
         # Because our 3x3 convs have padding size 1, they leave the input size unchanged.
         # The 2x2 max-pool divides the input size by 2. Flattening squares it.
@@ -86,7 +101,7 @@ class CNN(nn.Module):
         assert H == W == IMAGE_SIZE
         x = self.conv1(x)
         x = self.conv2(x)
-        x = self.max_pool(x)
+        # x = self.max_pool(x)
         x = self.dropout(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
