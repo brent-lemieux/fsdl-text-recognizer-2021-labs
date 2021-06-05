79e18f294f5499984867fe62c6e97d1c1abd16bd
Brent Lemieux <brent.lemieux@ibotta.com>
2021-06-05 14:54:41 -0600

    update

--- lab2/text_recognizer/models/cnn.py
+++ lab2/text_recognizer/models/cnn.py
@@ -89,7 +89,7 @@ class CNN(nn.Module):
         # Because our 3x3 convs have padding size 1, they leave the input size unchanged.
         # The 2x2 max-pool divides the input size by 2. Flattening squares it.
         conv_output_size = IMAGE_SIZE // (stride * 2)
-        print(conv_output_size)
+        print(f"CONV OUTPUT SIZE: {conv_output_size}")
         fc_input_dim = int(conv_output_size * conv_output_size * conv_dim)
         self.fc1 = nn.Linear(fc_input_dim, fc_dim)
         self.fc2 = nn.Linear(fc_dim, num_classes)
